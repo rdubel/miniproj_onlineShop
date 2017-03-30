ba0e698ba609f91d403d36ba8f50787519299c53
Rémy <remdub@live.fr>
2017-03-30 11:42:37 +0200

    Polish script and html

--- static/js/scriptcatalogue.js
+++ static/js/scriptcatalogue.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
 
-    for (var i = 0; i < catalog.length; i++) {
-        var art = $("<article></article>");
-        var img = $("<img>").attr({
-            src: catalog[i].thumb,
-            alt: 'img' + i
-        });
-        var prodname = $("<h5></h5>").html(catalog[i].name);
-        var prodesc = $("<p></p>").html(catalog[i].description.substr(0, 20) + "[...]");
-        var price = $("<p></p>").html(catalog[i].price + "€")
-        var btn = $("<a>Acheter</a>").attr({
-            class: 'btn',
-            href: 'file:///home/remy/Code/miniproj_onlineShop/produi1.html?nom=' + i
-        });
-        art.append(img, prodname, prodesc, price, btn);
-        $("#anch1").append(art);
+    $.ajax({
+        url: 'https://codi-e-commerce.herokuapp.com/',
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (catalog, status) {
+            for (var i in catalog) {
+                    var art = $("<article></article>");
+                    var img = $("<img>").attr({
+                        src: catalog[i].thumb,
+                        alt: 'img' + i
+                    });
+                    var prodname = $("<h5></h5>").html(catalog[i].name);
+                    var prodesc = $("<p></p>").html(catalog[i].description.substr(0, 20) + "[...]");
+                    var price = $("<p></p>").html(catalog[i].price + "€")
+                    var btn = $("<a>Acheter</a>").attr({
+                        class: 'btn',
+                        href: 'file:///home/remy/Code/miniproj_onlineShop/produi1.html?nom=' + i
+                    });
+                    art.append(img, prodname, prodesc, price, btn);
+                    $("#anch1").append(art);
+            }
+            $('#anch1').paginate({
+                perPage: 12
+            });
+        }
+    });
 
-    }
 
     $("#ex2").slider({});
-
     $("#ex2").change(function() {
         var values = $(this).val();
         var valarray = values.split(",")
@@ -32,9 +41,6 @@ $(document).ready(function() {
         url += "?" + pars;
     });
 
-    $('#anch1').paginate({
-        perPage: 12
-    });
 
     $('#chocoNoirs').addClass('active')
     $('#anch1').removeClass('cache')
--- static/js/scriptprudui.js
+++ static/js/scriptprudui.js
@@ -1,53 +1,61 @@
 $(document).ready(function() {
-
     var id = GET_PARAM("nom");
-    var cont = $(".carousel-inner")
 
-    var img = $("<img>").attr({
-        src: catalog[id].pictures[0],
-        alt: 'img1'
+    $.ajax({
+        url: 'https://codi-e-commerce.herokuapp.com/',
+        type: 'GET',
+        dataType: 'JSON',
+        success: function(catalog, status) {
+            var cont = $(".carousel-inner")
+
+            var img = $("<img>").attr({
+                src: catalog[id].pictures[0],
+                alt: 'img1'
+            });
+            var divimg = $("<div></div>").attr({
+                class: 'active item',
+                dataSlideNumber: 0
+            });
+            divimg.append(img);
+            cont.append(divimg)
+
+            for (var i = 1; i < catalog[id].pictures.length; i++) {
+                var img = $("<img>").attr({
+                    src: catalog[id].pictures[i],
+                    alt: 'img' + i
+                });
+                var divimg = $("<div></div>").attr({
+                    class: 'item',
+                    dataSlideNumber: i
+                });
+                divimg.append(img);
+                cont.append(divimg)
+
+                for (var i = 0; i < catalog[id].pictures.length; i++) {
+                    var thumbli = $("<li></li>").attr({
+                        class: 'col-sm-1'
+                    });
+                    var alink = $("<a></a>").attr({
+                        class: 'thumbnail',
+                        id: 'carousel-selector-' + i
+                    });
+                    var thumbimg = $("<img>").attr({
+                        src: catalog[id].pictures[i]
+                    });
+                    alink.append(thumbimg);
+                    thumbli.append(alink);
+                    $(".hide-bullets").append(thumbli)
+                }
+
+                $("h2").html(catalog[id].name)
+                $("h3").html("Quantitée : " + catalog[id].quantity)
+                $("h4").html("Prix : " + catalog[id].price + "€")
+                $(".descp").html(catalog[id].description)
+            }
+
+        }
     });
-    var divimg = $("<div></div>").attr({
-        class: 'active item',
-        dataSlideNumber: 0
-    });
-    divimg.append(img);
-    cont.append(divimg)
 
-    for (var i = 1; i < catalog[id].pictures.length; i++) {
-        var img = $("<img>").attr({
-            src: catalog[id].pictures[i],
-            alt: 'img' + i
-        });
-        var divimg = $("<div></div>").attr({
-            class: 'item',
-            dataSlideNumber: i
-        });
-        divimg.append(img);
-        cont.append(divimg)
-
-    }
-
-    for (var i = 0; i < catalog[id].pictures.length; i++) {
-        var thumbli = $("<li></li>").attr({
-            class: 'col-sm-1'
-        });
-        var alink = $("<a></a>").attr({
-            class: 'thumbnail',
-            id: 'carousel-selector-' + i
-        });
-        var thumbimg = $("<img>").attr({
-            src: catalog[id].pictures[i]
-        });
-        alink.append(thumbimg);
-        thumbli.append(alink);
-        $(".hide-bullets").append(thumbli)
-    }
-
-    $("h2").html(catalog[id].name)
-    $("h3").html("Quantitée : " + catalog[id].quantity)
-    $("h4").html("Prix : " + catalog[id].price + "€")
-    $(".descp").html(catalog[id].description)
 
     // When the carousel slides, auto update the text
     $('#myCarousel').on('slid.bs.carousel', function(e) {
